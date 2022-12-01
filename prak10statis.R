13c2683a894643e8932d1b6c6fc41518333e1510
theochrist <91509499+theochrist@users.noreply.github.com>
2022-12-01 13:37:07 +0700

    init

---
+++ prak10statis.R
@@ -0,0 +1,27 @@
+//latihan Pertama
+1-pbinom(3, 15, 0.1)
+
+pbinom(1, 15, prob=0.1)
+
+1-pbinom(6, 15, prob=0.9)
+1-pbinom(5, 15, prob=0.9)
+
+dbinom(10, 15, prob=0.9)
+
+dbinom(15, 15, prob=0.9)
+
+//Latihan Kedua
+dbinom(4, size=12, prob=0.2)
+
+dbinom(0, size= 12, prob=0.2) + dbinom(1, size=12, prob=0.2) + dbinom(2, size=12, prob=0.2) + dbinom(3, size=12, prob=0.2) + dbinom(4, size=12, prob=0.2)
+
+pbinom(4, size=12, prob=0.2)
+
+//latihan ketiga
+dbinom(5, 20, prob=0.9)
+
+dbinom(3, 20, prob=0.15)
+
+dbinom(8, 15, prob=0.85)
+
+dbinom(2, 15, prob=0.15)
